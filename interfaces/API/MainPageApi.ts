21b77dca831b20bbe14320125b47b39e321eb2c9
ArKravchenko <ar.kravchenko@gmail.com>
2022-02-08 18:43:34 +0300

    ListPage api mock added

--- interfaces/API/MainPageApi.ts
+++ interfaces/API/MainPageApi.ts
@@ -1,17 +1,14 @@
-import faker from '@faker-js/faker/locale/ru';
-import fs from 'fs'
-
-interface ImageSimple {
+export interface ImageSimple {
   src: string;
   alt: string;
 }
 
-interface ImageFull extends ImageSimple {
+export interface ImageFull extends ImageSimple {
   webpSrcSet: string | null;
   jpegSrcSet: string | null;
 }
 
-namespace Actualno {
+export namespace Actualno {
   enum ChangeTrend {
     positive = 1,
     negative = -1,
@@ -46,7 +43,7 @@ namespace Actualno {
   }
 }
 
-namespace SuperAnnounce {
+export namespace SuperAnnounce {
   export type Doc = {
     title: string;
     subtitle: string | null;
@@ -105,21 +102,19 @@ namespace SuperAnnounce {
     }
   }
 
-  export type SuperAnnounceData = null | IssueSA | ThemeSA | DocSA | CustomHtmlSA;
+  export type SuperAnnounceData =  null | IssueSA | ThemeSA | DocSA | CustomHtmlSA;
 
-  interface ISuperAnnounce {
+  export interface ISuperAnnounce {
     data: {
       primary: SuperAnnounceData;
       secondary: SuperAnnounceData
     }
   }
 
-  export interface APIDataStructure extends ISuperAnnounce {
-  }
-
+  export type APIDataStructure = SuperAnnounceData
 }
 
-namespace Top {
+export namespace Top {
 
   export type Doc = {
     title: string;
@@ -143,7 +138,7 @@ namespace Top {
 
 }
 
-namespace LightSpot {
+export namespace LightSpot {
 
 
   interface IQuote {
@@ -215,8 +210,10 @@ namespace LightSpot {
 
 }
 
-namespace MainToday {
+export namespace MainToday {
 
+  // TODO нужно добавить возможность выводить кастомное название главного + ссылку,
+  //  так как на разводящей  они настраиваются
   export type Doc = {
     title: string;
     subtitle: string;
@@ -237,9 +234,9 @@ namespace MainToday {
 
 }
 
-namespace Opinions {
+export namespace Opinions {
 
-  type Opinion = {
+  export type Opinion = {
     title: string;
     subtitle: string;
     tag: string;
@@ -258,7 +255,7 @@ namespace Opinions {
 
 }
 
-namespace Multimedia {
+export namespace Multimedia {
 
   export type MultimediaItem = {
     type: 'photo' | 'video' | 'podcast' | 'special' | 'visual'
@@ -280,7 +277,7 @@ namespace Multimedia {
 
 }
 
-namespace Rubrics {
+export namespace Rubrics {
 
   export type Doc = {
     title: string;
@@ -306,7 +303,7 @@ namespace Rubrics {
 
 }
 
-namespace PromoGalleryTop {
+export namespace PromoGalleryTop {
   export type PromoDoc = {
     title: string;
     subtitle: string;
@@ -326,12 +323,12 @@ namespace PromoGalleryTop {
 
 }
 
-namespace PromoGalleryBottom {
+export namespace PromoGalleryBottom {
   export interface APIDataStructure extends PromoGalleryTop.APIDataStructure {
   }
 }
 
-namespace CompanyNews {
+export namespace CompanyNews {
 
   export type NewsItem = {
     title: string;
@@ -353,7 +350,7 @@ namespace CompanyNews {
 
 }
 
-namespace MostReadable {
+export namespace MostReadable {
 
   export type Doc = {
     title: string;
@@ -374,7 +371,7 @@ namespace MostReadable {
 
 }
 
-namespace SpendTime {
+export namespace SpendTime {
 
   export type Doc = {
     title: string;
@@ -396,7 +393,7 @@ namespace SpendTime {
 
 }
 
-namespace Vote {
+export namespace Vote {
   /*
   Поля этого интерфейса еще под вопросом, так как нужно детальнее изучать работу опросов,
   Это за рамками прототипа
@@ -425,6 +422,64 @@ namespace Vote {
   }
 }
 
+export namespace RegionSideMenu {
+  /*
+  Поля этого интерфейса еще под вопросом, так как нужно детальнее изучать работу опросов,
+  Это за рамками прототипа
+   */
+
+  export type RegionSideMenuItem = {
+    title: string;
+    href: string;
+  }
+
+
+  interface IRegionSideMenu {
+    data: {
+      items: RegionSideMenuItem[]
+    }
+  }
+
+  export interface APIDataStructure extends IRegionSideMenu {
+  }
+}
+
+export namespace ListPageDocs {
+  /*
+  Поля этого интерфейса еще под вопросом, так как нужно детальнее изучать работу опросов,
+  Это за рамками прототипа
+   */
+
+  export type Tag = {
+    text: string;
+    href: string;
+  }
+
+  export type ListPageDoc = {
+    title: string;
+    subtitle: string;
+    href: string;
+    pubDate: Date;
+    img: ImageFull;
+    views: number;
+    comments: number;
+    tags: {
+      main: Tag;
+      rest: Tag[]
+    }
+  }
+
+
+  interface IListPageDocs {
+    data: {
+      timestamp: number;
+      docs: ListPageDoc[]
+    }
+  }
+
+  export interface APIDataStructure extends IListPageDocs {
+  }
+}
 
 export namespace MainPageAPI {
   export interface Endpoint_1 extends Actualno.APIDataStructure {
@@ -437,13 +492,19 @@ export namespace MainPageAPI {
   // interface Endpoint_3 extends Ads.APIDataStructure{}
 
   export interface Endpoint_4 {
-    superAnnounce: SuperAnnounce.APIDataStructure;
+    superAnnounce: {
+      data: {
+        primary: SuperAnnounce.SuperAnnounceData,
+        secondary: SuperAnnounce.SuperAnnounceData
+      }
+    };
     top: Top.APIDataStructure;
     ligthSpot: LightSpot.APIDataStructure;
     mainToday: MainToday.APIDataStructure;
     multimedia: Multimedia.APIDataStructure;
     rubrics: Rubrics.APIDataStructure;
     spendTime: SpendTime.APIDataStructure;
+    opinions: Opinions.APIDataStructure
   }
 
   export interface Endpoint_5 extends PromoGalleryTop.APIDataStructure {
@@ -462,411 +523,35 @@ export namespace MainPageAPI {
   }
 }
 
-const generateImageSimple = (): ImageSimple => {
-  return {
-    src: faker.image.imageUrl(),
-    alt: faker.lorem.sentence(5)
-  }
-}
-
-const generateImageFull = (): ImageFull => {
-  const srcSet = faker.image.imageUrl();
-  return {
-    src: faker.image.imageUrl(),
-    alt: faker.lorem.sentence(5),
-    jpegSrcSet: srcSet,
-    webpSrcSet: srcSet,
-  }
-}
-
-const randomLength = (min: number, max: number) => min + Math.round(Math.random() * (max - min))
-
-const generateEndpoint_1: () => MainPageAPI.Endpoint_1 = () => {
-
-  const generateActualnoItem = (): Actualno.ActualnoItem =>({
-    title: faker.lorem.sentence(randomLength(1, 5)),
-    href: faker.internet.url()
-  })
-
-  const generateActualno = (): Actualno.IActualno => {
-    return {
-      data: {
-        rates: {
-          href: faker.internet.url(),
-          usd: {
-            href: faker.internet.url(),
-            rate: faker.datatype.float({min: 70, max: 80, precision: 0.01}),
-            changeTrend: faker.datatype.number({min: -1, max: 1}),
-          },
-          eur: {
-            href: faker.internet.url(),
-            rate: faker.datatype.float({min: 80, max: 100, precision: 0.01}),
-            changeTrend: faker.datatype.number({min: -1, max: 1}),
-          },
-        },
-        items: Array.from({length: randomLength(5, 10)}).map(el => generateActualnoItem()) as ReturnType<typeof generateActualnoItem>[]
-      }
-    }
+export namespace ListPageAPI {
+  export interface Endpoint_1 extends Actualno.APIDataStructure {
   }
 
-  return generateActualno()
-}
+  // Лента не описывалась
+  // export interface Endpoint_2 extends Lenta.APIDataStructure{}
 
-const generateEndpoint_2: () => MainPageAPI.Endpoint_4 = () => {
+  // Реклама не описывалась
+  // interface Endpoint_3 extends Ads.APIDataStructure{}
 
-  const generateSa = (): SuperAnnounce.SuperAnnounceData => {
-    const Doc = (): SuperAnnounce.Doc => {
-      return {
-        title: faker.lorem.sentence(randomLength(3, 10)),
-        subtitle: faker.lorem.sentence(randomLength(10, 15)),
-        pubDate: faker.date.past(),
-        href: faker.internet.url(),
-      }
-    }
-
-    const Issue = (): SuperAnnounce.Issue => {
-      return {
-        title: faker.lorem.sentence(randomLength(1, 3)),
-        subtitle: faker.lorem.sentence(randomLength(3, 5)),
-        href: faker.internet.url(),
-      }
-    }
-
-    const Theme = (): SuperAnnounce.Theme => {
-      return {
-        title: faker.lorem.sentence(randomLength(1, 3)),
-        subtitle: faker.lorem.sentence(randomLength(3, 5)),
-        href: faker.internet.url(),
-      }
-    }
-    switch (faker.random.arrayElement(['doc', 'issue', 'theme', 'customHtml'])) {
-      case 'doc':
-        return {
-          type: 'doc',
-          content: {
-            plashka: faker.lorem.word(randomLength(3,10)),
-            img: generateImageSimple(),
-            mainDoc: Doc(),
-            docsList: Array.from({length: randomLength(1, 4)}).map(el => Doc()) as ReturnType<typeof Doc>[]
-          },
-        }
-      case 'issue':
-        return {
-          type: 'issue',
-          content: {
-            plashka: faker.lorem.word(randomLength(3,10)),
-            img: generateImageSimple(),
-            issue: Issue(),
-            docsList: Array.from({length: randomLength(1, 4)}).map(el => Doc()) as ReturnType<typeof Doc>[]
-          }
-        }
-      case 'theme':
-        return {
-          type: 'theme',
-          content: {
-            plashka: faker.lorem.word(randomLength(3,10)),
-            img: generateImageSimple(),
-            theme: Theme(),
-            docsList: Array.from({length: randomLength(1, 4)}).map(el => Doc()) as ReturnType<typeof Doc>[]
-          }
-
-        }
-      case 'customHtml':
-        return {
-          type: 'customHtml',
-          content: {
-            customHtml: `<div> ${faker.lorem.paragraph()} </div>`,
-            theme: Theme(),
-            docsList: Array.from({length: randomLength(1, 4)}).map(el => Doc()) as ReturnType<typeof Doc>[]
-          }
-        }
-    }
-    return null
+  interface Order {
+    order: number
   }
 
-  const generateTop = (): Top.APIDataStructure => {
-
-    const Doc = (): Top.Doc => {
-      return {
-        title: faker.lorem.sentence(randomLength(3, 10)),
-        exclusive: faker.datatype.boolean(),
-        href: faker.internet.url(),
-      }
-    }
-
-    return {
-      data: {
-        docs: Array.from({length: 10}).map(el => Doc()) as ReturnType<typeof Doc>[],
-        partner: {
-          title: faker.lorem.sentence(randomLength(3, 10)),
-          href: faker.internet.url(),
-        }
-      }
-    }
-
-  }
-
-  const generateLightSpot = (): LightSpot.APIDataStructure => {
-    const generateLightSpotItem = (): LightSpot.ILightSpotItem => {
-      switch (faker.random.arrayElement(['quote', 'photo', 'video', 'chart', 'digit'])) {
-        case 'quote':
-          return {
-            type: 'quote',
-            content: {
-              plashka: faker.lorem.word(randomLength(3,10)),
-              quote: faker.lorem.sentence(randomLength(5, 15)),
-              href: faker.internet.url(),
-              person: {
-                img: generateImageSimple(),
-                caption: faker.name.jobTitle(),
-              }
-            }
-          }
-        case 'photo':
-          return {
-            type: 'photo',
-            content: {
-              plashka: faker.lorem.word(randomLength(3,10)),
-              caption: faker.lorem.sentence(randomLength(5, 15)),
-              href: faker.internet.url(),
-              img: generateImageSimple(),
-            },
-          }
-        case 'video':
-          return {
-            type: 'video',
-            content: {
-              plashka: faker.lorem.word(randomLength(3,10)),
-              caption: faker.lorem.sentence(randomLength(5, 15)),
-              href: faker.internet.url(),
-              img: generateImageSimple(),
-            },
-          }
-        case 'chart':
-          return {
-            type: 'chart',
-            content: {
-              plashka: faker.lorem.word(randomLength(3,10)),
-              caption: faker.lorem.sentence(randomLength(5, 15)),
-              href: faker.internet.url(),
-              img: generateImageSimple(),
-            },
-          }
-        case 'digit':
-          return {
-            type: 'digit',
-            content: {
-              plashka: faker.lorem.word(randomLength(3,10)),
-              caption: faker.lorem.sentence(randomLength(5, 15)),
-              href: faker.internet.url(),
-              digit: `${faker.datatype.number()} ${faker.lorem.sentence(randomLength(1, 3))}`,
-            }
-          }
-      }
-      return null
-    }
-
-    return {
-      data: {
-        items: Array.from({length: randomLength(1, 10)}).map(el=>generateLightSpotItem()) as ReturnType<typeof generateLightSpotItem>[],
-      }
-    }
-  }
-
-  const generateMainToday = (): MainToday.APIDataStructure => {
-    const Doc = (): MainToday.Doc => ({
-      title: faker.lorem.sentence(randomLength(3, 10)),
-      subtitle: faker.lorem.sentence(randomLength(10, 15)),
-      tag: faker.lorem.word(),
-      href: faker.internet.url(),
-      img: generateImageFull(),
-    });
-    return {
-      data: {
-        docs: Array.from({length: 5}).map(el => Doc()) as ReturnType<typeof Doc>[]
-      }
-    }
-  }
-
-  const generateMultimedia = (): Multimedia.APIDataStructure => {
-    const MultimediaItem = (): Multimedia.MultimediaItem => ({
-      type: faker.random.arrayElement(['photo', 'video', 'podcast', 'special', 'visual']),
-      title: faker.lorem.sentence(randomLength(3, 10)),
-      subtitle: faker.lorem.sentence(randomLength(10, 15)),
-      tag: faker.lorem.word(),
-      href: faker.internet.url(),
-      img: generateImageSimple(),
-    });
-    return {
-      data: {
-        items: Array.from({length: randomLength(5, 10)}).map(el => MultimediaItem()) as ReturnType<typeof MultimediaItem>[]
-      }
-    }
-  }
-
-  const generateRubrics = (): Rubrics.APIDataStructure => {
-
-    const Doc = (): Rubrics.Doc => ({
-      title: faker.lorem.sentence(randomLength(3, 10)),
-      subtitle: faker.lorem.sentence(randomLength(10, 15)),
-      href: faker.internet.url(),
-      img: generateImageFull(),
-    })
-
-    const Rubric = (): Rubrics.Rubric => ({
-      title: faker.lorem.word(),
-      href: faker.internet.url(),
-      docs: Array.from({length: 5}).map(el => Doc()) as ReturnType<typeof Doc>[]
-    });
-    return {
-      data: {
-        rubrics: Array.from({length: 11}).map(el => Rubric()) as ReturnType<typeof Rubric>[]
-      }
-    }
-  }
-
-  const generateSpendTime = (): SpendTime.APIDataStructure => {
-
-    const Doc = (): SpendTime.Doc => ({
-      title: faker.lorem.sentence(randomLength(3, 10)),
-      subtitle: faker.lorem.sentence(randomLength(10, 15)),
-      href: faker.internet.url(),
-      img: generateImageFull(),
-      tag: faker.lorem.word(),
-    })
-
-    return {
-      data: {
-        docs: Array.from({length: 11}).map(el => Doc()) as ReturnType<typeof Doc>[]
-      }
-    }
-  }
-
-
-  return {
+  export interface Endpoint_4 {
+    timestamp: number;
     superAnnounce: {
-      data: {
-        primary: generateSa(),
-        secondary: generateSa()
-      }
-    },
-    top: generateTop(),
-    ligthSpot: generateLightSpot(),
-    mainToday: generateMainToday(),
-    multimedia: generateMultimedia(),
-    rubrics: generateRubrics(),
-    spendTime: generateSpendTime(),
-  }
-}
-
-const generateEndpoint_5_6: () => MainPageAPI.Endpoint_5 | MainPageAPI.Endpoint_6 = () => {
-
-  const PromoDoc = ():PromoGalleryTop.PromoDoc  => ({
-    title: faker.lorem.sentence(randomLength(3, 10)),
-    subtitle: faker.lorem.sentence(randomLength(10, 15)),
-    tag: faker.lorem.word(),
-    href: faker.internet.url(),
-    img: generateImageSimple()
-  })
-
-
-  const generatePromoGallery = (): PromoGalleryTop.APIDataStructure => {
-    return {
-      data: {
-        items: Array.from({length: 4}).map(el => PromoDoc()) as ReturnType<typeof PromoDoc>[]
-      }
-    }
+      data: SuperAnnounce.SuperAnnounceData
+    };
+    top: Top.APIDataStructure & Order;
+    ligthSpot: LightSpot.APIDataStructure & Order;
+    mainToday: MainToday.APIDataStructure & Order;
+    multimedia: Multimedia.APIDataStructure & Order;
+    opinions: Opinions.APIDataStructure & Order
   }
 
-  return generatePromoGallery()
-}
-
-const generateEndpoint_7: () => MainPageAPI.Endpoint_7 = () => {
-
-  const NewsItem = ():CompanyNews.NewsItem  => (
-    {
-      title: faker.lorem.sentence(randomLength(3, 10)),
-      subtitle: faker.lorem.sentence(randomLength(10, 15)),
-      href: faker.internet.url(),
-      pubDate: faker.date.past(),
-    })
-
-
-  const generateCompanyNews = (): CompanyNews.APIDataStructure => {
-    return {
-      data: {
-        title: faker.lorem.sentence(randomLength(3, 10)),
-        href: faker.internet.url(),
-        news: Array.from({length: 4}).map(el => NewsItem()) as ReturnType<typeof NewsItem>[]
-      }
-    }
+  export interface Endpoint_5 extends RegionSideMenu.APIDataStructure{
   }
 
-  return generateCompanyNews()
-}
-
-const generateEndpoint_8: () => MainPageAPI.Endpoint_8 = () => {
-
-  const Answer = (): Vote.Answer => ({
-    text: faker.lorem.sentence(randomLength(3, 10)),
-    value: faker.datatype.number({min: 0, max: 9999}),
-    name: faker.lorem.word(5),
-  })
-
-
-  const generateVote = (): Vote.APIDataStructure => {
-    return {
-      data: {
-        resultsHref: faker.internet.url(),
-        question: faker.lorem.sentence(randomLength(3, 10)).replace('.', '?'),
-        questionId: faker.datatype.number({min: 1000, max: 9999}),
-        regionId: faker.datatype.number({min: 0, max: 99}),
-        uid: faker.datatype.number({min: 0, max: 9999}),
-        redirectUrlOnClose: faker.internet.url(),
-        answers: Array.from({length: randomLength(1, 10)}).map(el => Answer()) as ReturnType<typeof Answer>[]
-      }
-    }
+  export interface Endpoint_6 extends ListPageDocs.APIDataStructure{
   }
-
-  return generateVote()
 }
-
-const generateEndpoint_9: () => MainPageAPI.Endpoint_9 = () => {
-
-  const Doc = (): MostReadable.Doc => (
-    {
-      title: faker.lorem.sentence(randomLength(3, 10)),
-      href: faker.internet.url(),
-      img: generateImageFull(),
-      views: faker.datatype.number({min: 0, max: 9999}),
-    })
-
-
-  const generateMostReadable = (): MostReadable.APIDataStructure => {
-    return {
-      data: {
-        docs: Array.from({length: randomLength(1, 10)}).map(el => Doc()) as ReturnType<typeof Doc>[]
-      }
-    }
-  }
-
-  return generateMostReadable()
-}
-
-
-
-fs.writeFileSync('./MainPageApi_mock.json', JSON.stringify({
-  Endpoint_1: generateEndpoint_1(),
-  Endpoint_2: generateEndpoint_2(),
-  Endpoint_5: generateEndpoint_5_6(),
-  Endpoint_6: generateEndpoint_5_6(),
-  Endpoint_7: generateEndpoint_7(),
-  Endpoint_8: generateEndpoint_8(),
-  Endpoint_9: generateEndpoint_9(),
-}))
-
-
-
-
-
-
