21c11167d766099b848a3b38ebc3f7da9e1e06c4
ArKravchenko <ar.kravchenko@gmail.com>
2022-07-20 13:52:06 +0300

    customHtmlSa added

--- components/MainPage/SuperAnnounce/SaComponent/CustomHtmlSa/CustomHtmlSa.ts
+++ components/MainPage/SuperAnnounce/SaComponent/CustomHtmlSa/CustomHtmlSa.ts
@@ -1,9 +1,13 @@
 import {Component, Prop, Vue} from 'nuxt-property-decorator'
 import type {SuperAnnounce} from "~/interfaces/API/MainPageApi";
+import LazyHydrate from "vue-lazy-hydration";
 
 
-
-@Component({})
+@Component({
+  components:{
+    LazyHydrate
+  }
+})
 export default class CustomHtmlSa extends Vue {
   @Prop({
     // type: Object,
@@ -14,9 +18,81 @@ export default class CustomHtmlSa extends Vue {
     // }
   }) customHtmlSaData!: SuperAnnounce.ICustomHtmlSA | null;
 
-  cdnUrl: string = process.env.CDN_URL || ''
+  // cdnUrl: string = process.env.CDN_URL || ''
+
+  get getDocs() {
+    return this.customHtmlSaData?.content?.docsList?.length
+    && this.customHtmlSaData.content.docsList
+  }
+
+  formatDate(date: Date){
+    return new Date(date).toLocaleDateString('ru-ru',
+      {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute:'numeric'
+      })
+  }
+
+  get getRawHtml(){
+    return this.customHtmlSaData?.content?.customHtml?.length
+    && this.customHtmlSaData.content.customHtml
+  }
+
+  // runScripts() {
+  //   if (!this.getRawHtml) return
+  //
+  //   const fakeElement = document.createElement('div');
+  //   fakeElement.innerHTML = this.getRawHtml;
+  //
+  //   const scriptsContainer = <Element>this.$refs.scripts
+  //   let scripts = (fakeElement as Element).querySelectorAll('script');
+  //
+  //   const scriptsWithSrc = Array.from(scripts).filter(el => el.src)
+  //   const scriptsWithInnerHtml = Array.from(scripts).filter(el => el.innerHTML)
+  //
+  //
+  //   const loadedScripts = Array.from(scriptsWithSrc).map(script => {
+  //     return new Promise(resolve => {
+  //       const scriptElement = document.createElement('script');
+  //       //@ts-ignore
+  //       [...script.attributes].forEach(attr => {
+  //         scriptElement.setAttribute(attr.nodeName, attr.nodeValue)
+  //         // console.log(attr.nodeName, attr.nodeValue)
+  //       })
+  //       scriptElement.setAttribute('defer', 'true')
+  //       scriptElement.onload = function () {
+  //         resolve(null)
+  //       };
+  //
+  //       scriptsContainer.appendChild(scriptElement)
+  //       script.remove()
+  //     })
+  //   })
+  //
+  //   Promise.all(loadedScripts).then(() => {
+  //     Array.from(scriptsWithInnerHtml).forEach(script => {
+  //       const scriptElement = document.createElement('script');
+  //       //@ts-ignore
+  //       [...script.attributes].forEach(attr => {
+  //         scriptElement.setAttribute(attr.nodeName, attr.nodeValue)
+  //       })
+  //       scriptElement.setAttribute('defer', 'true')
+  //       scriptElement.innerHTML = script.innerHTML
+  //       scriptsContainer.appendChild(scriptElement)
+  //       script.remove()
+  //     })
+  //   })
+  // }
 
   mounted(){
+    // try {
+    //   this.runScripts()
+    // } catch (e) {
+    //   console.error(e)
+    // }
     //  console.log('this.customHtmlSaData', this.customHtmlSaData)
   }
 }
--- components/MainPage/SuperAnnounce/SaComponent/CustomHtmlSa/CustomHtmlSa.vue
+++ components/MainPage/SuperAnnounce/SaComponent/CustomHtmlSa/CustomHtmlSa.vue
@@ -1,8 +1,35 @@
 <template>
 
-  <div>
-    <h1>CustomHtmlSa</h1>
-    <div>{{JSON.stringify(customHtmlSaData)}}</div>
+  <div class="sa">
+    <div class="sa__cover sa__cover--widget" v-if="getRawHtml">
+      <LazyHydrate never>
+        <div class="sa__custom" v-html="getRawHtml" ref="scripts">
+          <!--          <div style="background: #004465;color: #fff;display: flex;align-items: center;justify-content: center;height: 250px;font-size: 3rem;">Кастомный виджет</div>-->
+        </div>
+      </LazyHydrate>
+    </div>
+    <div class="sa__chapter sa__chapter--widget" v-if="getDocs">
+      <ul class="sa__list">
+        <template v-for="(doc, index) in getDocs">
+          <li class="sa__item" :key="index">
+            <h4 class="sa__time sa__time--widget">
+              <a :href="doc.href" class="link">{{ formatDate(doc.pubDate) }}</a>
+            </h4>
+            <h2 class="sa__subname sa__subname--widget">
+              <a :href="doc.href" class="link link--overlay">
+                {{doc.title}}
+              </a>
+            </h2>
+          </li>
+
+        </template>
+        <li class="sa__item sa__item--more sa__item--story" v-if="customHtmlSaData.content.theme">
+          <h2 class="sa__subname sa__subname--widget">
+            <a :href="customHtmlSaData.content.theme.href" class="link">Весь сюжет</a>
+          </h2>
+        </li>
+      </ul>
+    </div>
   </div>
 
 </template>
